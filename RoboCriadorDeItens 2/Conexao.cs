0b2b1a744a2d223a8f673923507a01ce3defa3e9
AndreGRabello <79485297+AndreGRabello@users.noreply.github.com>
2021-02-24 17:27:59 -0300

    Update Conexao.cs

--- RoboCriadorDeItens 2/Conexao.cs
+++ RoboCriadorDeItens 2/Conexao.cs
@@ -27,6 +27,48 @@ namespace RoboCriadorDeItens_2
                 }
                 return crmServiceClient;
             }
+          
+        
+            /*        
+            private static CrmServiceClient crmServiceClientOrigem;
+            private static CrmServiceClient crmServiceClientDestino;
+
+            public CrmServiceClient ObterConexaoOrigem()
+            {
+                    var connectionStringCRM = @"AuthType=OAuth;
+                    Username = admin@fyitcc.onmicrosoft.com;
+                    Password = NaN==false; SkipDiscovery = True;
+                    AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                    RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                    Url = https://fyitccv1.api.crm.dynamics.com/XRMServices/2011/Organization.svc;";
+
+
+                    if (crmServiceClientOrigem == null)
+                    {
+                        System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    crmServiceClientOrigem = new CrmServiceClient(connectionStringCRM);
+                    }
+                    return crmServiceClientOrigem;
+            }
+
+            public CrmServiceClient ObterConexaoDestino()
+            {
+                var connectionStringCRM = @"AuthType=OAuth;
+                    Username = adminrobo@adminrobo.onmicrosoft.com;
+                    Password = TCCabc123321*; SkipDiscovery = True;
+                    AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                    RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                    Url = https://tccg03.api.crm2.dynamics.com/XRMServices/2011/Organization.svc;";
+
+
+                if (crmServiceClientDestino == null)
+                {
+                    System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    crmServiceClientDestino = new CrmServiceClient(connectionStringCRM);
+                }
+                return crmServiceClientDestino;
+            }
+            */
     }
 }
 
