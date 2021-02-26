524e4f7bb86ced28d3c24ee50409859953c632ac
Filipe <filipe_oli@hotmail.com>
2021-02-25 21:43:10 -0300

    Arramando a bagaça

--- RoboCriadorDeItens 2/Program.cs
+++ RoboCriadorDeItens 2/Program.cs
@@ -9,15 +9,14 @@ using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Client;
 
+
 namespace RoboCriadorDeItens_2
 {
     class Program
     {
-        public static int quantidade = 1;
         static void Main(string[] args)
         {
-            RoboDeCriacao.criacao();
-            Console.WriteLine("Criado com Sucesso!");
+            Console.WriteLine("Sucesso!!!");
             Console.ReadLine();
         }
     }
