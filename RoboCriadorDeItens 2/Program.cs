cc0cd2df7750daf23cbd5b404b10df0e3ab63659
Filipe de Oliveira Barbosa <76171152+filipe0li@users.noreply.github.com>
2021-03-06 04:39:34 -0300

    Alteração na interface.
    Criar teste de plugin para apresentação.

--- RoboCriadorDeItens 2/Program.cs
+++ RoboCriadorDeItens 2/Program.cs
@@ -6,31 +6,34 @@ namespace RoboCriadorDeItens_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Deseja Cria ou Importar? C/I");
-            string resposta = Console.ReadLine().ToLower();
-            bool start = true;
-            while (start)
-            {
-                switch (resposta)
-                {
-                    case "c":
-                        RoboDeCriacao.criacao();
-                        start = false;
-                        break;
-
-                    case "i":
-                        RoboDeImportacao.importacao();
-                        start = false;
-                        break;
-                    default:
-                        Console.WriteLine("Digite uma alternativa válida!");
-                        start = true;
-                        resposta = Console.ReadLine().ToLower();
-                        break;
-                }
-            }
-            Console.WriteLine("Sucesso!!!");
-            Console.ReadLine();
-        }
+		bool start = true;
+		while (start)
+		{
+			Console.WriteLine("Deseja Criar, Importar, Plugin ou Sair? (C / I / P / S)");
+			string resposta = Console.ReadLine().ToLower();
+			Console.Clear();
+			switch (resposta)
+			{
+				case "c":
+					RoboDeCriacao.criacao();
+					Console.WriteLine("Sucesso!");
+					break;
+				case "i":
+					RoboDeImportacao.importacao();
+					Console.WriteLine("Sucesso!");
+					break;
+				case "p":
+					Console.WriteLine("Plugin");
+					Console.WriteLine("Sucesso!!");
+					break;
+				case "s":
+					start = false;
+					break;
+				default:
+					Console.WriteLine("Alternativa inválida!");
+					break;
+			}
+		}
+		Console.WriteLine("FIM!");
     }
 }
